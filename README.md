e02648e9dc5c03222a416c45d99a465b36d1fa27
Ghostgogogo <2762193314@qq.com>
2022-12-29 16:33:25 +0800

    第一章

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # C6
 《C Primer Plus》第6版学习仓库
+
+学习约定
+
+1. 笔记使用Windows自带的记事本
+2. 使用VS Code书写所有代码【cha01-cha16】
+3. 向Github上传代码
---
+++ ch01/1.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Concrete contains gravel and cement.\n");
+
+    getchar();
+
+    return 0;
+}
